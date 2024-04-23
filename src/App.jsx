6298fa932e16c991214a2e5d9c48307765691fd6
HuraiAH <huraiah17@gmail.com>
2024-04-23 11:31:51 +0600

    Fixed the app builds  error

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-// import { useEffect, useState } from "react";
-// import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
 import "./App.css";
-// import authService from "./appwrite/authservice";
-import { Footer } from "./components/footer/Footer";
-// import { logOut, login } from "./store/authSlice";
+import authService from "./appwrite/authservice";
+import { Footer, Header } from "./components/index";
+import { logOut, login } from "./store/authSlice";
 
-export function App() {
-  // const [loading, setLoading] = useState(true);
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   authService
-  //     .getCurrentUser()
-  //     .then((userData) => {
-  //       if (userData) {
-  //         dispatch(login(userData));
-  //       } else {
-  //         dispatch(logOut());
-  //       }
-  //     })
-  //     .finally(() => setLoading(false));
-  // });
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logOut());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
 
-  return (
+  return !loading ? (
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
         <Footer />
       </div>
     </div>
-  );
+  ) : null;
 }
 
-// export default App;
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <>
+      <h1>hi..</h1>
+    </>
+  );
+}
+export default Header;
