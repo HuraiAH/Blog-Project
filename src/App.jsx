24b2705f198986e8ed6d63a57f4ad196deab609f
HuraiAH <huraiah17@gmail.com>
2024-04-23 09:43:35 +0600

    added some feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+// import { useEffect, useState } from "react";
+// import { useDispatch } from "react-redux";
 import "./App.css";
-import authService from "./appwrite/authservice";
-import { Footer, Header } from "./components/index";
-import { logOut, login } from "./store/authSlice";
+// import authService from "./appwrite/authservice";
+import { Footer } from "./components/footer/Footer";
+// import { logOut, login } from "./store/authSlice";
 
-function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login(userData));
-        } else {
-          dispatch(logOut());
-        }
-      })
-      .finally(() => setLoading(false));
-  });
+export function App() {
+  // const [loading, setLoading] = useState(true);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   authService
+  //     .getCurrentUser()
+  //     .then((userData) => {
+  //       if (userData) {
+  //         dispatch(login(userData));
+  //       } else {
+  //         dispatch(logOut());
+  //       }
+  //     })
+  //     .finally(() => setLoading(false));
+  // });
 
-  return !loading ? (
+  return (
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
-        <Header />
-        <h1>hiii</h1>
-        <main>{/* TODO: <Outlet /> */}</main>
         <Footer />
       </div>
     </div>
-  ) : null;
+  );
 }
 
-export default App;
+// export default App;
